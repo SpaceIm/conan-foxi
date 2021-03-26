84c293dcaacc29618b7f87504274b83456f3e307
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-26 01:54:36 +0100

    update test package

--- test_package/test_package.c
+++ test_package/test_package.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 
 int main() {
-    struct onnxifi_library *core = NULL;
-    int ret = onnxifi_load(ONNXIFI_LOADER_FLAG_VERSION_1_0, NULL, core);
+    struct onnxifi_library onnx;
+    int ret = onnxifi_load(ONNXIFI_LOADER_FLAG_VERSION_1_0, NULL, &onnx);
     if (!ret) {
         printf("Cannot load onnxifi lib\n");
         return 0;
     }
-    onnxifi_unload(core);
+    onnxifi_unload(&onnx);
     return 0;
 }
