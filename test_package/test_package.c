1465b27e8dbfd2389890d39e496b1504aa800ede
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-26 01:32:19 +0100

    initialize core

--- test_package/test_package.c
+++ test_package/test_package.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int main() {
-    struct onnxifi_library *core;
+    struct onnxifi_library *core = NULL;
     int ret = onnxifi_load(ONNXIFI_LOADER_FLAG_VERSION_1_0, NULL, core);
     if (!ret) {
         printf("Cannot load onnxifi lib\n");
