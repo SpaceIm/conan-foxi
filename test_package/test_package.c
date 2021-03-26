5f36b38d1016d389ac5eb206435a9ad26ae1c6e9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-26 01:16:26 +0100

    add test package

--- test_package/test_package.c
+++ test_package/test_package.c
@@ -1,3 +1,14 @@
+#include <foxi/onnxifi_loader.h>
+
+#include <stdio.h>
+
 int main() {
+    struct onnxifi_library *core;
+    int ret = onnxifi_load(ONNXIFI_LOADER_FLAG_VERSION_1_0, NULL, core);
+    if (!ret) {
+        printf("Cannot load onnxifi lib\n");
+        return 0;
+    }
+    onnxifi_unload(core);
     return 0;
 }
